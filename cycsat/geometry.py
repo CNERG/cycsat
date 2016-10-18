0c02291e8fa1508f79fafa18916f46af5d336634
Owen Selles <oaselles@gmail.com>
2016-10-18 11:27:11 -0500

    notebook demo

--- cycsat/geometry.py
+++ cycsat/geometry.py
@@ -1,48 +1,18 @@
-'''
+"""
 geometry.py
 
-'''
-
-import shapely
-import json
-
-from skimage.draw import polygon
+"""
 from shapely.geometry import Polygon, Point
-
-import shapely
-import numpy as np
 
 building = Polygon([(300, 300), (480, 320), (380, 430), (220, 590)])
 
-class Canvas(object):
+def build_site():
 	'''
+	Builds a site by fixing the placement of all it's features and giving them geometries.
+
+	for feature in site.features:
+		check if overlap
+		place feature
+
 	'''
-
-	def __init__(self,length,width,bands):
-		'''
-		'''
-		self.data = np.zeros((length, width),dtype=np.uint8)
-
-
-	def add_feature(self,feature,rgb):
-		'''
-		Add a geometry to a canvas, returns new canvas
-		'''
-		coords = np.array(list(feature.exterior.coords))
-		rr, cc = polygon(coords[:,0], coords[:,1], self.data.shape)
-
-		self.data[rr, cc] = rgb
-
-
-
-'''
-adding a feature to a site will involve placing that feature so that it makes sense (eg no overlap)
-
-turbine building - rectangle
-containment building - circle
-cooling tower - circle
-
-'''
-
-
-
+	pass
--- cycsat/sensor.py
+++ cycsat/sensor.py
@@ -1,12 +1,44 @@
-"""
+'''
 sensor.py
 
-Contains tools for writing scenes.
+Object/API for writing scenes from pre-built geometries.
 
-"""
+'''
+
+import shapely
+import json
+
+from skimage.draw import polygon
+from shapely.geometry import Polygon, Point
+
+import shapely
+import numpy as np
 
 import gdal
-import numpy as np
+
+
+class Image(object):
+	'''
+	'''
+
+	def __init__(self,length,width,bands=3):
+		'''
+		'''
+		self.bands = dict()
+		for band in range(bands):
+			self.bands[band+1] = np.zeros((length, width),dtype=np.uint8)
+
+	def add_feature(self,feature,spectra):
+		'''
+		Add a geometry to a canvas, returns new canvas
+		'''
+
+		coords = np.array(list(feature.exterior.coords))
+		rr, cc = polygon(coords[:,0], coords[:,1], self.bands[1].shape)
+
+		for band in zip(self.bands, spectra):
+			self.bands[band[0]][rr, cc] = band[1]
+
 
 extensions = {
     'GTiff':'.tif'
@@ -17,20 +49,26 @@ extensions = {
 # 2. get all the features from the site that are "static"
 
 
-def array_to_image(path,array,image_format='GTiff'):
+def array_to_image(image,path,image_format='GTiff'):
 
-    rows = array.shape[-2]
-    cols = array.shape[-1]
+    rows = image.bands[1].shape[-2]
+    cols = image.bands[1].shape[-1]
+
+    bands = len(image.bands)
 
     driver = gdal.GetDriverByName(image_format)
 
     # add file extension based on driver
-    outRaster = driver.Create(path+extensions[image_format], cols, rows, 1, gdal.GDT_Byte)
+    outRaster = driver.Create(path+extensions[image_format], cols, rows, bands, gdal.GDT_Byte)
 
-    # write bands
-    outband = outRaster.GetRasterBand(1)
-    outband.WriteArray(array)
-    outband.FlushCache()
+    for band in image.bands:
+    	outband = outRaster.GetRasterBand(band)
+    	outband.WriteArray(image.bands[band])
+    	outband.FlushCache()
+
+
+
+
 
 
 
