89253ed01f447a58a4fc170854e85b19ed11c6d4
Owen Selles <oaselles@gmail.com>
2017-04-01 15:12:24 -0500

    start up

--- sample.py
+++ sample.py
@@ -14,8 +14,11 @@ from cycsat.simulation import CycSat
 from cycsat.archetypes import Facility, Feature, Shape, Rule
 from cycsat.prototypes.reactor import SampleReactor
 
+SampleReactor = ()
+
+
 templates = {
-	'Reactor' : SampleReactor(),
+	'Reactor' : SampleReactor,
 }
 
 sim = CycSat('reactor_test_sample.sqlite')
