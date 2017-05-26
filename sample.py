1a04e53bdd33825f64e459de26e7c1142f8a1f96
Owen Selles <oaselles@gmail.com>
2017-05-25 23:01:42 -0500

    demo update

--- sample.py
+++ sample.py
@@ -1,15 +1,3 @@
-import os
-from shutil import copyfile
-import random
-
-# copying the test database (this is just for repeated testing)
-src = 'C:/Users/Owen/Documents/Academic/CNERG/cycsat/simulations/four_reactors.sqlite'
-dst = 'C:/Users/Owen/Documents/Academic/CNERG/cycsat/reactor_test_sample.sqlite'
-copyfile(src, dst)
-
-# =============================================================================
-# TESTING CYCSAT STARTS HERE
-# =============================================================================
 from cycsat.simulation import Simulator
 from cycsat.prototypes.ByronIL import ByronIL
 from cycsat.laboratory import USGSMaterial
