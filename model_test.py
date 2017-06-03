7ed0a161181bb59ac0960b8071a0c3be32a63937
Owen Selles <oaselles@gmail.com>
2017-06-02 19:30:13 -0500

    logging systems

--- model_test.py
+++ model_test.py
@@ -1,5 +1,6 @@
 from modeler.model import Simulation, Surface, Agent
 
+
 # must be a list of Surface objects
 surfaces = [Surface(np.ones((100, 100), dtype='int8'))]
 
@@ -11,3 +12,19 @@ timesteps = pd.date_range('1/1/2015', '1/31/2015')
 
 s = Simulation(surfaces, agents, timesteps)
 s.run()
+
+from geopandas import GeoSeries
+
+
+# class Species:
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class Organism(Species):
+
+#     def __init__(self, name):
+#         Species.__init__(self, name=name)
+
+# o = Organism('test')
--- modeler/model.py
+++ modeler/model.py
@@ -13,6 +13,9 @@ import inspect
 import xarray as xr
 from matplotlib import pyplot as plt
 
+from geopandas import GeoSeries
+from pandas import pandas
+
 
 class Simulation:
 
@@ -26,17 +29,19 @@ class Simulation:
         timesteps : series of timesteps
 
         """
-        self.timesteps = pd.Series(timesteps)
+        self.timesteps = pd.DataFrame(
+            {'label': timesteps, 'run': np.full((timesteps.size), False)})
         self.surfaces = pd.Series(surfaces)
         self.agents = pd.Series(agents)
 
     def data(self):
         """Combine the agent logs."""
-        agent_logs = list()
+        agent_data = list()
         for i, agent in enumerate(self.agents):
-            agent_log = agent.log
-            agent_logs.append(agent_log.assign(agent=i))
-        return pd.concat(agent_logs)
+            agent_data.append(agent.data.assign(agent=i))
+        agent_data = pd.concat(agent_data).reset_index()
+        agent_data.rename(str, columns={'index': 'timestep'}, inplace=True)
+        return agent_data
 
     def plot(self, ax=None, surfaces=False, column=None):
         if ax:
@@ -54,10 +59,15 @@ class Simulation:
     def run(self):
         """Initializes all agents and initializes the simulation."""
 
+        # initial agents as the first timestep simulation run
         self.clear_agents()
         self.agents.apply(lambda x: x.init(self))
-        for timestep in self.timesteps[1:]:
-            self.agents.apply(lambda x: x.run(self))
+        self.timesteps.set_value(0, 'run', True)
+
+        # run simulations for remaining timesteps
+        for i, timestep in self.timesteps[1:].iterrows():
+            self.agents.apply(lambda x: x.master_run(self, i))
+            self.timesteps.set_value(i, 'run', True)
 
     def clear_agents(self):
         """Clears the data log for all agents."""
@@ -81,11 +91,20 @@ class Surface:
         return ax.imshow(self.data)
 
 
+# class AgentGroup:
+
+
 class Agent:
 
     def __init__(self):
         # log of updates
-        self.log = gpd.GeoDataFrame()
+        self.metadata = pd.DataFrame()
+        self.data = gpd.GeoDataFrame()
+
+    def master_run(self, simulation, timestep):
+        """Returns a dictionary the defines the agent's attributes at a single point in time."""
+        data, metadata = self.run(simulation)
+        self.record(data)
 
     def plot(self, ax=None, column=None):
         if ax:
@@ -93,26 +112,40 @@ class Agent:
         else:
             fig, ax = plt.subplots(1)
             ax.set_aspect('equal')
-        return self.log.plot(ax=ax, column=column)
+        return self.data.plot(ax=ax, column=column)
 
-    def record(self, data):
-        self.log = self.log.append(data, ignore_index=True)
+    def record(self, data=None):
+        """Records attributes."""
+        self.metadata = self.metadata.append({}, ignore_index=True)
+        if data:
+            data = gpd.GeoDataFrame(index=[len(self.metadata)], data=data)
+            self.data = self.data.append(data)
+        else:
+            pass
 
     def init(self, simulation):
+        """Returns a dictionary that represents the agents atttributes at a particular point
+        in time."""
+
         # example place randomly on Map
         minx, miny, maxx, maxy = simulation.surfaces[0].box.bounds
         x = random.randint(minx, maxx)
         y = random.randint(miny, maxy)
 
-        self.log = self.log.append(
-            {'geometry': Point(x, y).buffer(1),
-             'value': 0}, ignore_index=True)
+        self.record(
+            {'geometry': Point(x, y).buffer(1), 'value': 0})
 
     def run(self, simulation):
+        """Must return two dictonaries {data} and {metadata}."""
+
         # randomly decide the direction
         xoff = random.choice([-1, 1]) * 1
         yoff = random.choice([-1, 1]) * 1
 
+        if random.choice([True, False]):
+            return {}, {}
+
         # simulate a timestep
-        self.record({'geometry': translate(self.log.geometry.iloc[-1], xoff, yoff),
-                     'value': np.random.normal(0)})
+        return {'geometry': translate(self.data.geometry.iloc[-1], xoff, yoff),
+                'value': self.data.value.iloc[-1] + np.random.normal(0),
+                'max': self.data.value.iloc[-1] * self.data.value.max()}, {}
