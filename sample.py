ab9591c97b3901e44ffd79f3324b44edca0dbf21
Owen Selles <oaselles@gmail.com>
2017-06-14 14:04:08 -0500

    adding an example of placement

--- sample.py
+++ sample.py
@@ -2,5 +2,24 @@ from cycsat.agent import Agent
 from shapely.geometry import Polygon, box, Point
 
 # initalize agents with random values
-site = Agent(geometry=box(0, 0, 100, 100))
-site.sub_agents = [Agent(geometry=Point(0, 0).buffer(10))]
+site = Agent(geometry=box(0, 0, 500, 500))
+
+parking_lot1 = Agent(geometry=box(0, 0, 100, 100))
+parking_lot2 = Agent(geometry=box(0, 0, 100, 100))
+
+site.sub_agents = [parking_lot1, parking_lot2]
+
+for pl in site.sub_agents:
+    pl.place(site.geometry)
+
+parking_lot1.sub_agents = [
+    Agent(geometry=Point(0, 0).buffer(5)) for i in range(100)]
+
+for a in parking_lot1.sub_agents:
+    a.place(parking_lot1.geometry)
+
+parking_lot2.sub_agents = [
+    Agent(geometry=Point(0, 0).buffer(5)) for i in range(100)]
+
+for a in parking_lot2.sub_agents:
+    a.place(parking_lot2.geometry)
