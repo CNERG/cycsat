eae008474db8e812fa6713d9be2f809ed3f7551f
oaselles <oaselles@gmail.com>
2017-06-14 12:24:30 -0500

    restart

--- refresh.py
+++
@@ -1,6 +0,0 @@
-from shutil import copyfile
-
-# copying the test database (this is just for repeated testing)
-src = 'C:/Users/Owen/Documents/Academic/CNERG/cycsat/simulations/four_reactors.sqlite'
-dst = 'C:/Users/Owen/Documents/Academic/CNERG/cycsat/reactor_test_sample.sqlite'
-copyfile(src, dst)
