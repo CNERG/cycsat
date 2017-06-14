ab384eeb8dd8fd557d4ac8d9ae20f528aa8a296f
oaselles <oaselles@gmail.com>
2017-06-14 13:06:47 -0500

    placement from archetypes

--- cycsat/agent.py
+++ cycsat/agent.py
@@ -20,7 +20,25 @@ class Agent:
             log = log.append(sa.data.tail(1), ignore_index=True)
         return log
 
-    def place(self, mask):
+    def place(self, region, attempts=100):
+
+        for i in range(attempts):
+            posit_point(mask)
+
+            typology_checks = list()
+
+            loc = shape.place(posited_point, simulation, timestep)
+
+            typology_checks.append(loc.geometry.within(results['restrict']))
+
+            if False not in typology_checks:
+                self.site.build.database.save(self)
+                self.morph(simulation, timestep)
+                return True
+
+            print(
+                self.name, 'placement failed after {', attempts, '} attempts.')
+            return False
 
     print('point placement failed after {', attempts, '} attempts.')
     return False
