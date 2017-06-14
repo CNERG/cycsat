d036965a45a08c26f6b7231da5d3654155c876df
oaselles <oaselles@gmail.com>
2017-06-14 12:36:01 -0500

    new start

--- sample.py
+++ sample.py
@@ -1,41 +1,8 @@
-from cycsat.simulation import Simulator
-from cycsat.prototypes.ByronIL import ByronIL
-from cycsat.laboratory import USGSMaterial
-from cycsat.prototypes.instrument import Blue, Red, Green
+from cycsat.archetypes import Agent
 
-from matplotlib import pyplot as plt
+# initalize agents with random values
+agents = pd.Series([Agent(value=i) for i in range(100)])
 
-#------------------------------------------------------------------------
-# Define a Reactor
-#------------------------------------------------------------------------
-
-db = Simulator('reactor_test_sample.sqlite')
-
-temps = {'Reactor1': ByronIL}
-b = db.create_build(temps)
-s = b.simulate(end=6)
-site = b.sites[0]
-
-s.plot(5)
-
-# # b = db.load_build(1)
-# # s = b.simulations[0]
-# # site = b.sites[0]
-
-# fig, axes = plt.subplots(1, 5)
-# axes = axes.flatten()
-
-# for i in range(5):
-#     site.assemble()
-#     site.plot(ax=axes[i])
-
-# fig, axes = plt.subplots(1, 3)
-# for i, inst in enumerate([Blue, Green, Red]):
-#     print('plotting', inst)
-#     sensor = inst()
-#     sensor.calibrate(site)
-#     sensor.mmu = 150
-#     sensor.plot(ax=axes[i], simulation=s, timestep=5)
-#     sensor.write(str(i))
-
-# axes[0].imshow(plt.imread('byronIL.PNG'))
+# generate 100 steps of test data
+for i in range(100):
+    i = agents.apply(lambda x: x.run())
