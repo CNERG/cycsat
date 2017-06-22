c5f2738fb728c0ef9390314374ef7e01744566b9
Owen Selles <oaselles@gmail.com>
2017-06-22 16:32:23 -0500

    parents do work

--- cycsat/agent.py
+++ cycsat/agent.py
@@ -14,12 +14,18 @@ from shapely.affinity import rotate, translate
 
 class Agent:
 
-    def __init__(self, **args):
+    def __init__(self, **variables):
         # clear the log and add init arguments
         self.data = GeoDataFrame()
-        self.step = 0
-        self.log(step=int(self.step), **args)
-        self.agents = list()
+        self.time = 0
+        self.variables = variables
+        self.log(**variables)
+        self.parent = False
+        self.__agents__ = list()
+
+    @property
+    def agents(self):
+        return pd.Series(self.__agents__)
 
     @property
     def subdata(self):
@@ -29,18 +35,67 @@ class Agent:
             log = log.append(agent.subdata, ignore_index=True)
         return log
 
-    def log(self, **args):
-        """Record information about the agent."""
+    def add_agents(self, agents):
+        if type(agents) is list:
+            for agent in agents:
+                agent.parent = self
+                self.agents.append(agent)
+        else:
+            agent.parent = self
+            self.agents.append(agent)
 
-        # set and log initial attributes
+    def log(self, **args):
+        """Log the agent's variables."""
+
+        # if new args are added
         for arg in args:
             setattr(self, arg, args[arg])
+            self.variables[arg] = args[arg]
 
-        # archive attributes
-        self.data = self.data.append(args, ignore_index=True)
+        # get a snapshot of varibles
+        log = {}
+        for var in self.variables:
+            log[var] = getattr(self, var)
 
-    def surface(self, value_field):
-        """Generates a blank raster surface."""
+        # append the log to dataframe
+        self.data = self.data.append(log, ignore_index=True)
+
+    def run(self, **args):
+        """Evaluates the __run__ function and runs through sub agents."""
+        self.time += 1
+
+        try:
+            result = self.__run__()
+            if result:
+                self.log(**result)
+        except:
+            pass
+
+        for sub_agent in self.agents:
+            sub_agent.run()
+
+    def place_in(self, region, attempts=100):
+        """Places in a given region."""
+        # bounding region of parent agent
+        for i in range(attempts):
+            placement = posit_point(region, attempts=attempts)
+            if placement:
+                x, y = [placement.coords.xy[0][
+                    0], placement.coords.xy[1][0]]
+                _x, _y = [self.geometry.centroid.coords.xy[0][
+                    0], self.geometry.centroid.coords.xy[1][0]]
+                shift_x = x - _x
+                shift_y = y - _y
+
+                geometry = translate(
+                    self.geometry, xoff=shift_x, yoff=shift_y)
+                if geometry.within(region):
+                    return geometry
+        return False
+
+
+     def surface(self, value_field):
+        """Generates a blank raster surface using the provided value field."""
         minx, miny, maxx, maxy = [round(coord)
                                   for coord in self.geometry.bounds]
 
@@ -66,27 +121,8 @@ class Agent:
 
         return image
 
-    def run(self, parent_agent=None, steps=1, **args):
-        """Evaluates the USER DEFINED EVALUATE function and runs through sub a gents."""
-
-        for step in range(steps):
-            self.step = self.step + 1
-
-            try:
-                result = self.__run__(parent_agent=parent_agent, **args)
-            except:
-                result = None
-
-            if result:
-                self.log(step=int(self.step), **result)
-            else:
-                pass
-
-            for sub_agent in self.agents:
-                sub_agent.run(parent_agent=self)
-
-    # def __run__(self, **args):
-    #     """USER DEFINED. Return a dictionary of attributes to record, or None."""
+    def __run__(self, **args):
+        """DEFINED. Returns a dictionary of attributes."""
 
     def evaluate(self, parent_agent=None):
 
@@ -108,27 +144,7 @@ class Agent:
         """
         return parent_agent.geometry
 
-    def place_in(self, region, attempts=100):
-        """Places the agent within another agent's geometry."""
-        # bounding region of parent agent
-
-        for i in range(attempts):
-            placement = posit_point(region, attempts=attempts)
-            if placement:
-                x, y = [placement.coords.xy[0][
-                    0], placement.coords.xy[1][0]]
-                _x, _y = [self.geometry.centroid.coords.xy[0][
-                    0], self.geometry.centroid.coords.xy[1][0]]
-                shift_x = x - _x
-                shift_y = y - _y
-
-                geometry = translate(
-                    self.geometry, xoff=shift_x, yoff=shift_y)
-                if geometry.within(region):
-                    return geometry
-        return False
-
-    def setup(self, parent_agent=None, attempts=100):
+    def setup(self, attempts=100):
         """Places the agent and all of it's sub agents randomly into a provided region.
 
         Parameters
@@ -139,8 +155,8 @@ class Agent:
 
         self.data = self.data.head(1)
 
-        if parent_agent:
-            geometry = self.place_in(parent_agent.geometry)
+        if self.parent:
+            geometry = self.place_in(self.parent.geometry)
         else:
             geometry = self.geometry
 
--- sample.py
+++ sample.py
@@ -4,46 +4,43 @@ from cycsat.laboratory import Material
 from shapely.geometry import Polygon, box, Point
 import random
 
+a = Agent()
 
-class Site(Agent):
+# class Site(Agent):
 
-    def __init__(self):
-        Agent.__init__(self)
-        self.geometry = box(0, 0, 500, 500)
-
-
-class CoolingTower(Agent):
-
-    def __init__(self):
-        Agent.__init__(self)
-        self.geometry = Point(0, 0).buffer(50)
-        self.name = 'cooling tower'
+#     def __init__(self):
+#         Agent.__init__(self)
+#         self.geometry = box(0, 0, 500, 500)
 
 
-class Plume(Agent):
+# class CoolingTower(Agent):
 
-    def __init__(self):
-        Agent.__init__(self)
-        self.geometry = Point(0, 0).buffer(25)
-        self.name = 'plume'
+#     def __init__(self):
+#         Agent.__init__(self)
+#         self.geometry = Point(0, 0).buffer(50)
+#         self.name = 'cooling tower'
 
-    def __evaluate__(self, **args):
 
-        if args['parent_agent'].on:
-            return args['parent_agent'].geometry.buffer(100)
+# class Plume(Agent):
 
-    def __run__(self, **args):
-        valid = self.evaluate(args['parent_agent'])
-        spawn_zone = valid.centroid.buffer(50)
+#     def __init__(self):
+#         Agent.__init__(self)
+#         self.geometry = Point(0, 0).buffer(25)
+#         self.name = 'plume'
 
-        puffs = [Agent(geometry=Point(0, 0).buffer(25))]
+#     def __evaluate__(self, **args):
 
-        return {'geometry': geo}
+#         if args['parent_agent'].on:
+#             return args['parent_agent'].geometry.buffer(100)
 
-site = Site()
-ctower = CoolingTower()
+#     def __run__(self, **args):
+#         valid = self.evaluate(args['parent_agent'])
+#         spawn_zone = valid.centroid.buffer(50)
 
-plume = Plume()
 
-ctower.agents = [plume]
-site.agents = [ctower]
+# site = Site()
+# ctower = CoolingTower()
+# plume = Plume()
+
+# ctower.agents = [plume]
+# site.agents = [ctower]
