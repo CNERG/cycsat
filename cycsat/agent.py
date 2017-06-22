06dbcf8255c35b85a03b6e0bf7b0c6807bf6db30
Owen Selles <oaselles@gmail.com>
2017-06-22 17:49:48 -0500

    imporved logging

--- cycsat/agent.py
+++ cycsat/agent.py
@@ -19,16 +19,21 @@ class Agent:
         self.data = GeoDataFrame()
         self.time = 0
         self.variables = variables
-        self.log(**variables)
         self.parent = False
+        self.log(init=True)
         self.__agents__ = list()
 
     @property
     def agents(self):
-        return pd.Series(self.__agents__)
+        """Collects the current attributes of all subagents."""
+        agent_info = GeoDataFrame()
+        for agent in self.__agents__:
+            agent_info = agent_info.append(
+                agent.data.tail(1), ignore_index=True)
+        return agent_info
 
     @property
-    def subdata(self):
+    def get_data_tree(self):
         """Collects the current attributes of all agents by cascading."""
         log = self.data.tail(1)
         for agent in self.agents:
@@ -44,20 +49,32 @@ class Agent:
             agent.parent = self
             self.agents.append(agent)
 
-    def log(self, **args):
-        """Log the agent's variables."""
-
-        # if new args are added
+    def add_variables(self, **args):
+        """Adds a new variable to track in the log."""
         for arg in args:
             setattr(self, arg, args[arg])
-            self.variables[arg] = args[arg]
 
-        # get a snapshot of varibles
-        log = {}
-        for var in self.variables:
-            log[var] = getattr(self, var)
+        if self.variables:
+            self.variables.update(args)
+        else:
+            self.variables = args
 
-        # append the log to dataframe
+    def log(self, init=False):
+        """Log the agent's variables."""
+
+        if init:
+            for var in self.variables:
+                setattr(self, var, self.variables[var])
+            log = self.variables
+        else:
+            log = {}
+            for var in self.variables:
+                log[var] = getattr(self, var)
+
+        if self.parent:
+            log['time'] = self.parent.time
+        else:
+            log['time'] = self.time
         self.data = self.data.append(log, ignore_index=True)
 
     def run(self, **args):
@@ -65,14 +82,26 @@ class Agent:
         self.time += 1
 
         try:
-            result = self.__run__()
-            if result:
-                self.log(**result)
+            active = self.__run__()
+            if active:
+                self.log()
         except:
             pass
 
         for sub_agent in self.agents:
             sub_agent.run()
+
+    def place(self, attempts=100):
+        """Places the agent and all of it's sub_agents.
+
+        Parameters
+        ----------
+        agent - the agent to place the shape within
+        attempts - the attempts before the placement fails
+        """
+
+        for sub_agent in self.agents:
+            sub_agent.setup(parent_agent=self, attempts=attempts)
 
     def place_in(self, region, attempts=100):
         """Places in a given region."""
@@ -93,79 +122,35 @@ class Agent:
                     return geometry
         return False
 
+    #  def surface(self, value_field):
+    #     """Generates a blank raster surface using the provided value field."""
+    #     minx, miny, maxx, maxy = [round(coord)
+    #                               for coord in self.geometry.bounds]
 
-     def surface(self, value_field):
-        """Generates a blank raster surface using the provided value field."""
-        minx, miny, maxx, maxy = [round(coord)
-                                  for coord in self.geometry.bounds]
+    #     image = np.zeros((maxx - minx, maxy - miny)) + \
+    #         getattr(self, value_field)
 
-        image = np.zeros((maxx - minx, maxy - miny)) + \
-            getattr(self, value_field)
+    #     return image
 
-        return image
+    # def collect_surfaces(self, value_field, image=[], res=1):
 
-    def collect_surfaces(self, value_field, image=[], res=1):
+    #     # if no image is provided create a blank
+    #     if len(image) == 0:
+    #         image = self.surface(value_field)
+    #         # else add the agent to the image
+    #     else:
+    #         coords = np.array(list(self.geometry.exterior.coords))
+    #         rr, cc = polygon(coords[:, 0], coords[:, 1], image.shape)
+    #         image[rr, cc] = getattr(self, value_field)
 
-        # if no image is provided create a blank
-        if len(image) == 0:
-            image = self.surface(value_field)
-            # else add the agent to the image
-        else:
-            coords = np.array(list(self.geometry.exterior.coords))
-            rr, cc = polygon(coords[:, 0], coords[:, 1], image.shape)
-            image[rr, cc] = getattr(self, value_field)
+    #     if self.agents:
+    #         for agent in self.agents:
+    #             image = agent.collect_surfaces(value_field, image=image)
 
-        if self.agents:
-            for agent in self.agents:
-                image = agent.collect_surfaces(value_field, image=image)
+    #     return image
 
-        return image
-
-    def __run__(self, **args):
-        """DEFINED. Returns a dictionary of attributes."""
-
-    def evaluate(self, parent_agent=None):
-
-        # if this is the top agent return its geometry
-        if not parent_agent:
-            return False
-
-        try:
-            result = self.__evaluate__(parent_agent)
-            if 'shapely' in fullname(result):
-                return result
-            else:
-                return parent_agent.geometry
-        except:
-            return parent_agent.geometry
-
-    def __evaluate__(self, parent_agent, **args):
-        """USER DEFINED. Given a parent agent, evaluates the placement relative to all other agents.
-        """
-        return parent_agent.geometry
-
-    def setup(self, attempts=100):
-        """Places the agent and all of it's sub agents randomly into a provided region.
-
-        Parameters
-        ----------
-        agent - the agent to place the shape within
-        attempts - the attempts before the placement fails
-        """
-
-        self.data = self.data.head(1)
-
-        if self.parent:
-            geometry = self.place_in(self.parent.geometry)
-        else:
-            geometry = self.geometry
-
-        if geometry:
-            self.data = self.data.set_value(0, 'geometry', geometry)
-            self.geometry = geometry
-
-        for sub_agent in self.agents:
-            sub_agent.setup(parent_agent=self, attempts=attempts)
+    # def __run__(self, **args):
+    #     """DEFINED. Returns a dictionary of attributes."""
 
 
 def posit_point(mask, attempts=1000):
@@ -185,21 +170,10 @@ def fullname(o):
     return o.__module__ + "." + o.__class__.__name__
 
 
-# class SpatialRule:
+class SpatialRule:
 
-#     def __init__(self):
-#         pass
+    def __init__(self):
+        """A defined set of instructions (function) for returning."""
 
-#     @property
-#     def targets(self, agents, attr=None):
-#         """Finds the targets given a list."""
-
-
-# class NEAR(SpatialRule):
-
-#     def __init__(self, target, value):
-#         self.target = target
-#         self.value = value
-#         pass
-
-#     def run(self):
+    def evaluate(self):
+        self.run.__evaluate__(to_place, target_list)
