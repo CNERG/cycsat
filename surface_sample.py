aa845d2e6d89a1ea9663c05c3251c95c7635ac2b
Owen Selles <oaselles@gmail.com>
2017-07-03 20:19:30 -0500

    experimenting with surface filters

---
+++ surface_sample.py
@@ -0,0 +1,28 @@
+from cycsat.agent import Agent
+from cycsat.laboratory import Material, USGSMaterial
+
+from shapely.geometry import Polygon, box, Point
+import random
+
+
+class CtrlPt(Agent):
+
+    def __init__(self):
+        geo = Point(0, 0).buffer(5)
+        val = random.randint(-500, 500)
+
+        Agent.__init__(self, geometry=geo, value=val)
+
+    def __run__(self):
+        self.value += (np.random.normal(self.value) * random.choice([-1, 1]))
+
+surface = Agent(geometry=box(0, 0, 500, 500), value=0)
+pts = [CtrlPt() for i in range(200)]
+
+surface.add_agents(pts)
+surface.place()
+
+from scipy.ndimage.filters import gaussian_filter
+out = surface.render('value')
+
+a = gaussian_filter(out, sigma=10)
