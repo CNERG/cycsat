3253de63818382658eef086933e7a3c7833a0654
Owen Selles <oaselles@gmail.com>
2017-07-07 16:58:28 -0500

    ignoring data, clean ignore

--- bin/build_materials.py
+++ bin/build_materials.py
@@ -1,4 +1,14 @@
 """
 A script for building the materials database from the
-USGS Spectral Library (https://www.sciencebase.gov/catalog/item/5807a2a2e4b0841e59e3a18d).
+USGS Spectral Library (https://www.sciencebase.gov/catalog/item/586e8c88e4b0f5ce109fccae)
 """
+import urllib.request
+
+print('Downloading USGS spectral library...')
+url = 'http://www.sciencebase.gov/catalog/file/get/586e8c88e4b0f5ce109fccae/?f=__disk__a7%2F4f%2F91%2Fa74f913e0b7d1b8123ad059e52506a02b75a2832'
+filename = '/cycsat/data/materials.zip'
+urllib.request.urlretrieve(url, filename)
+
+
+if __name__ == "__main__":
+    pass
